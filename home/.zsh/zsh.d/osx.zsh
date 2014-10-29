398ed571ff63e1b2761af76147a46da983b21ea0
Jakub Nawalaniec <jakub.nawalaniec@getbase.com>
2014-10-29 14:13:26 +0100

    Convenience aliases

--- home/.zsh/zsh.d/osx.zsh
+++ home/.zsh/zsh.d/osx.zsh
@@ -10,3 +10,19 @@ else # crappy osx ls installed
 fi
 unalias tmux
 alias tmux="SHELL=/usr/local/bin/zsh tmux"
+export STARTER_KIT_VM_ROOT='/opt/starter-kit'
+export STARTER_KIT_CACHE_ROOT='/opt/starter-kit'
+export STARTER_KIT_SHARED_FOLDER_ROOT='/opt/base/'
+
+STARTER_KIT_RUBY_VERSION='2.1.2'
+
+alias starter-kit="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit"
+alias spo="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit provision --only"
+alias s="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit remote s"
+alias sr="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit remote"
+alias se="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit remote se"
+alias sc="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit remote sc"
+alias si="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION starter-kit remote si"
+alias grid="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION grid"
+alias dropship="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION dropship"
+alias dss="RBENV_VERSION=$STARTER_KIT_RUBY_VERSION dropship ssh"
