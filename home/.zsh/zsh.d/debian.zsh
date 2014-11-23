a474ac6235afb2d3a55f1c970b2f1e98f58c028b
pielgrzym <pielgrzym@prymityw.pl>
2014-11-23 18:01:14 +0100

    Debian specific stuff handled the right way

--- home/.zsh/zsh.d/debian.zsh
+++ home/.zsh/zsh.d/debian.zsh
@@ -1,5 +1,3 @@
-if $(command -v apt-get 2>&1 > /dev/null); then
-    alias upgrade="sudo apt-get update && sudo apt-get upgrade"
-    alias update="sudo apt-get update"
-    alias aptf="apt-cache search"
-fi
+alias upgrade="sudo apt-get update && sudo apt-get upgrade"
+alias update="sudo apt-get update"
+alias aptf="apt-cache search"
