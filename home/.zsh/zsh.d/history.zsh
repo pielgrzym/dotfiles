254aecd8ffd513ea3432ca2b7416a5aa1d4ebf50
Jakub Nawalaniec <jakub.nawalaniec@getbase.com>
2014-11-25 17:41:54 +0100

    Reenable incremental history file update

--- home/.zsh/zsh.d/history.zsh
+++ home/.zsh/zsh.d/history.zsh
@@ -8,7 +8,7 @@ setopt hist_ignore_dups # ignore duplication command history list
 unsetopt share_history # by some queer accident it was turned on (horror!)
 
 setopt hist_verify
-#setopt inc_append_history
+setopt inc_append_history
 setopt extended_history
 setopt hist_expire_dups_first
 setopt hist_find_no_dups
