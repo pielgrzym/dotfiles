08a7c28c357d4ac54aef09a94ebdfcec7efd9240
Jakub Nawalaniec <jakub.nawalaniec@getbase.com>
2015-02-14 16:23:32 +0100

    Use gnu date/sort on osx

--- home/.zsh/zsh.d/osx.zsh
+++ home/.zsh/zsh.d/osx.zsh
@@ -3,6 +3,8 @@ if type gls > /dev/null; then # osx gnu coreutils installed
     alias ls='\gls -h --color=auto'
     alias ll='\gls -lh --color=auto'
     alias l='\gls -h -1 --color=auto'
+    alias date='gdate'
+    alias sort='gsort'
 else # crappy osx ls installed
     alias ls="ls -h -G"
     alias ll='ls -lh -G'
