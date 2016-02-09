ec81c2d904989a7b896176a026559ea7154b6b2a
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2016-02-09 23:41:00 +0100

    Use gpg-agent instead of ssh-agent

--- home/.zsh/zsh.d/keychain.zsh
+++ home/.zsh/zsh.d/keychain.zsh
@@ -1,9 +1,18 @@
-if (( ${+commands[keychain]} )); then
-    if [[ -d $HOME/.ssh ]]; then
-        if [[ -f $HOME/.ssh/id_dsa ]]; then
-            eval `keychain --eval --nogui -Q -q ~/.ssh/id_dsa`
-        elif [[ -f $HOME/.ssh/id_rsa ]]; then
-            eval `keychain --eval --nogui -Q -q ~/.ssh/id_rsa`
-        fi
-    fi
+# if (( ${+commands[keychain]} )); then
+#     if [[ -d $HOME/.ssh ]]; then
+#         if [[ -f $HOME/.ssh/id_dsa ]]; then
+#             eval `keychain --eval --nogui -Q -q ~/.ssh/id_dsa`
+#         elif [[ -f $HOME/.ssh/id_rsa ]]; then
+#             eval `keychain --eval --nogui -Q -q ~/.ssh/id_rsa`
+#         fi
+#     fi
+# fi
+if [[ $(gconftool-2 --get /apps/gnome-keyring/daemon-components/ssh) != "false" ]]; then
+  gconftool-2 --type bool --set /apps/gnome-keyring/daemon-components/ssh false
+fi
+if [ -f "${HOME}/.gpg-agent-info"  ]; then
+    . "${HOME}/.gpg-agent-info"
+    export GPG_AGENT_INFO
+    export SSH_AUTH_SOCK
+    export SSH_AGENT_PID
 fi
